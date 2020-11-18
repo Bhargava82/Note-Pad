fb4472da5e3bb76d0cd5a436a0b9384c5c40c195
Bhargava Patel <bhargavapatel82@gmail.com>
2020-11-18 12:01:14 -0500

    added the index.js file reinstalled the packages.

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+
+// VIEWS ROUTES
+//GET `/notes` - Should return the `notes.html` file.
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "notes"));
+});
+
+//GET `*` - Should return the `index.html` file
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index"));
+});
+
+//API routes
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+// app.get('/api/tables', (req, res) => {
+//   res.json({reservations: reservations,
+//     waitlist: waitlist})
+// })
+
+// app.post('/api/tables', (req, res) => {
+//   reservations.push(req.body)
+//   res.json({reservations: reservations,
+//     waitlist: waitlist})
+// })
